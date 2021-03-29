815b26629a6203d60450f37b861df297edaed86b
shmalens <zivaka00@gmail.com>
2021-03-30 01:43:32 +0300

    Разобрался с валгриндом, поправил тревис

--- run_valgrind.sh
+++ run_valgrind.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-valgrind --tool=memcheck --leak-check=full --track-origins=yes  --show-leak-kinds=all ./parallel
+valgrind --tool=memcheck --child-silent-after-fork=yes --leak-check=full --track-origins=yes  --show-leak-kinds=all ./parallel
 valgrind --tool=memcheck --leak-check=full --track-origins=yes  --show-leak-kinds=all ./consistent
